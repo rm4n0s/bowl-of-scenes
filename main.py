acee90476975b9b41313544aa04c21627a6944a4
Manos Ragiadakos <manos.ragiadakos@gmail.com>
2025-12-04 21:10:27 +0200

    starting project, added GPLv3 license and uv's dependency manager

---
+++ main.py
@@ -0,0 +1,10 @@
+# Copyright © 2025-2026 Emmanouil Ragiadakos
+# SPDX-License-Identifier: GPL-3.0
+
+
+def main():
+    print("Hello from bowl-of-scenes!")
+
+
+if __name__ == "__main__":
+    main()
